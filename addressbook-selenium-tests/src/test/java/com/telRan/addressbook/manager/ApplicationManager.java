cac43f31463ba7141a76d439f5811f8a5d65fee5
Olya <Olya@github.com>
2018-11-12 21:07:34 +0200

    12/11/18

--- addressbook-selenium-tests/src/test/java/com/telRan/addressbook/manager/ApplicationManager.java
+++ addressbook-selenium-tests/src/test/java/com/telRan/addressbook/manager/ApplicationManager.java
@@ -2,18 +2,37 @@ package com.telRan.addressbook.manager;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.BrowserType;
 
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
+  private  String browser;
   WebDriver wd;
   private  SessionHelper sessionHelper;
   private  GroupHelper groupHelper;
   private NavigationHelper navigationHelper;
 
+  public ApplicationManager(String browser) {
+        this.browser = browser;
+  }
+
   public void start() {
-    wd = new ChromeDriver();
+
+    if (browser.equals(BrowserType.CHROME)) {
+      wd = new ChromeDriver();
+      if(browser.equals(BrowserType.FIREFOX)) {
+        wd = new FirefoxDriver();
+      }else if(browser.equals(BrowserType.EDGE)){
+        wd = new EdgeDriver();
+      }else if(browser.equals(BrowserType.IE)){
+        wd = new EdgeDriver();
+      }
+    }
     wd.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+
     navigationHelper = new NavigationHelper(wd);
     navigationHelper.openSite("http://localhost/addressbook");
     sessionHelper = new SessionHelper(wd);
@@ -24,11 +43,9 @@ public class ApplicationManager {
 
   }
 
-
   public void stop() {
     wd.quit();
   }
-
 
   public GroupHelper getGroupHelper() {
     return groupHelper;
--- addressbook-selenium-tests/src/test/java/com/telRan/addressbook/tests/TestBase.java
+++ addressbook-selenium-tests/src/test/java/com/telRan/addressbook/tests/TestBase.java
@@ -1,6 +1,7 @@
 package com.telRan.addressbook.tests;
 
 import com.telRan.addressbook.manager.ApplicationManager;
+import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
@@ -8,7 +9,7 @@ import org.testng.annotations.BeforeSuite;
 
 public class TestBase {
 
-  protected static ApplicationManager app = new ApplicationManager();
+  protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
 
   @BeforeSuite
   public void setUp() {
